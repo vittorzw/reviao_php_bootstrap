b001dc4c6e7b50604850bab9f532566f6beebbd6
vitor <hugov3141v@gmail.com>
2024-02-09 16:22:51 -0300

    a

--- conexao.php
+++ conexao.php
@@ -1,19 +1,37 @@
 <?php
 
+// // Conexão (MySql)
+
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $dbname = "cadastro";
+
+
+// try{
+   
+
+    
+//     $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
+
+// }  catch(PDOException $err){
+    
+// }
+
+// Conexão (Postgre)
+
 $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "cadastro";
-// $port = 3306;
+
 
 try{
-    //Conexão com a porta
-    // $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+   
 
-    //Conexão sem a porta
+    
     $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
 
-    //echo "Conexão com banco de dados realizado com sucesso!";
 }  catch(PDOException $err){
-    //echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $err->getMessage();
+    
 }
